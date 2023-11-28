8a9d28ab8d9ba1fd7248937387792939e746095f
Paulhakeem <paulnyamawi18@gmail.com>
2023-11-28 19:06:03 +0300

    creating a login page

---
+++ components/HomePage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    class="bg-gradient-to-r from-[#7283ef] to-[#4ffb8d] bg-cover bg-center mx-6 rounded-md text-white"
+  >
+    <div class="flex flex-col-2 flex-wrap my-8 gap-8 justify-evenly pt-6">
+      <div class="">
+        <h1
+          class="text-4xl font-semibold select-none tracking-wide first-letter:uppercase text-[#23283b]"
+        >
+          welcome to our budget app
+        </h1>
+        <p class="pt-4 w-96 first-letter:uppercase text-gray-700">
+          our app helps you to track your money you spending on
+          day to day activites.
+        </p>
+        <icons />
+      </div>
+
+      <div class="text-left">
+        <h1
+          class="text-xl font-semibold select-none tracking-wide first-letter:uppercase text-[#23283b]"
+        >
+          create an account
+        </h1>
+        <p class="first-letter:uppercase text-gray-700">
+          create your account and start tacking your money.
+        </p>
+      <Forms/>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script setup></script>
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#00dd82',
+        'text-color': '#7b869c'
+      }
+    },
+  },
+  plugins: [],
+}
+
