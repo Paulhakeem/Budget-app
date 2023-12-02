49f01d1f67f45adad00bba08acd466c149e90272
Paulhakeem <paulnyamawi18@gmail.com>
2023-12-02 15:50:47 +0300

    adding validation

--- components/Forms.vue
+++ components/Forms.vue
@@ -18,55 +18,66 @@
           <div>
             <h1 class="text-2xl font-semibold text-gray-700">SignUp</h1>
           </div>
-          <div class="divide-y divide-gray-200">
-            <div
-              class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7"
-            >
-              <div class="relative">
-                <input
-                  autocomplete="off"
-                  id="email"
-                  name="email"
-                  type="text"
-                  class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
-                  placeholder="Email address"
-                />
-                <label
-                  for="email"
-                  class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
-                  >Email Address</label
-                >
-              </div>
-              <div class="relative">
-                <input
-                  autocomplete="off"
-                  id="password"
-                  name="password"
-                  type="password"
-                  class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
-                  placeholder="Password"
-                />
-                <label
-                  for="password"
-                  class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
-                  >Password</label
-                >
-              </div>
-              <div class="relative">
-                <nuxt-link to="/Home/">
-                  <button
-                    class="bg-gradient-to-bl from-[#7283ef] to-[#4ffb8d] text-white rounded-md px-2 py-1 w-full"
-                  >
-                    Submit
-                  </button>
-                </nuxt-link>
-              </div>
+          <!-- form -->
+          <Form
+            @submit="onSubmit"
+            :validation-schema="schema"
+            class="divide-gray-200 mt-6"
+          >
+            <div class="relative">
+              <Field
+                id="email_addr"
+                name="email_addr"
+                type="email"
+                class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                placeholder="Email address"
+              />
+              <label
+                for="email"
+                class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                >Email Address</label
+              >
             </div>
-          </div>
+            <ErrorMessage name="email_addr" class="text-red-600" />
+
+            <div class="relative mt-4">
+              <Field
+                id="password"
+                name="acc_pazzword"
+                type="password"
+                class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                placeholder="Password"
+              />
+              <label
+                for="password"
+                class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                >Password</label
+              >
+              <ErrorMessage name="acc_pazzword" class="text-red-600" />
+            </div>
+
+            <button
+              class="bg-gradient-to-bl from-[#7283ef] to-[#4ffb8d] text-white rounded-md px-2 py-1 w-full mt-4"
+            >
+              Submit
+            </button>
+          </Form>
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { Field, Form, ErrorMessage } from "vee-validate";
+import * as Yup from "yup";
+
+const schema = Yup.object().shape({
+  email_addr: Yup.string().email().required().label("Email Address"),
+  acc_pazzword: Yup.string().min(5).required().label("Your Password"),
+});
+
+function onSubmit(values) {
+  alert(JSON.stringify(values, null, 2));
+}
+</script>
